bfd33ee41a38531a07ad0b6c6b3317a962d025e3
sevagrigorev <sevagriogryev@gmail.com>
2021-05-14 14:21:22 +0300

    Call "all" a separate method

--- src/main/java/RainbowBot.java
+++ src/main/java/RainbowBot.java
@@ -10,85 +10,104 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class RainbowBot extends org.telegram.telegrambots.bots.TelegramLongPollingBot{
+public class RainbowBot extends org.telegram.telegrambots.bots.TelegramLongPollingBot {
 
     public void onUpdateReceived(Update update) {
+        AllTags(update);
+        pdrBot(update);
+    }
 
-            if (update.hasMessage()) {
-                Message message = update.getMessage();
+    private void pdrBot(Update update) {
+        if (update.hasMessage()) {
+            Message message = update.getMessage();
 
-                  if(message.hasText()) {
-                  String text = message.getText();
+            if (message.hasText()) {
+                String text = message.getText();
 
-                    if (text.equals("/all")) {
-                        SendMessage sendMessage = new SendMessage();
-                        sendMessage.setText("ПОГНАЛИ В РАДУГУ \uD83C\uDF08 ПАЧАНЫ: ");
-//                        sendMessage.setText("ПОГНАЛИ В РАДУГУ ПАЧАНЫ\n @LtNice @VmiakoV @Rekane @faraboh @grSeva");
-                        sendMessage.setParseMode(ParseMode.MARKDOWN);
-                        sendMessage.setChatId(message.getChatId());
+            }
+        }
+    }
 
-                        //InlineKeyboardButton
-                        InlineKeyboardMarkup inlineKeyboardMarkup = new InlineKeyboardMarkup();
-                        List<List<InlineKeyboardButton>> inlineButtonsList = new ArrayList<>();
-                        List<InlineKeyboardButton> keyboardButtonsRow1 = new ArrayList<>();
-                        List<InlineKeyboardButton> keyboardButtonsRow2 = new ArrayList<>();
-                        List<InlineKeyboardButton> keyboardButtonsRow3 = new ArrayList<>();
+    private void AllTags(Update update) {
+        if (update.hasMessage()) {
+            Message message = update.getMessage();
 
-                        InlineKeyboardButton inlineKeyboardButton1 = new InlineKeyboardButton();
-                        InlineKeyboardButton inlineKeyboardButton2 = new InlineKeyboardButton();
-                        InlineKeyboardButton inlineKeyboardButton3 = new InlineKeyboardButton();
-                        InlineKeyboardButton inlineKeyboardButton4 = new InlineKeyboardButton();
-                        InlineKeyboardButton inlineKeyboardButton5 = new InlineKeyboardButton();
-                        InlineKeyboardButton inlineKeyboardButton6 = new InlineKeyboardButton();
+            if (message.hasText()) {
+                String text = message.getText();
 
-                        keyboardButtonsRow1.add(inlineKeyboardButton1.setText("Дениска").setCallbackData("@LtNice"));
-                        keyboardButtonsRow1.add(inlineKeyboardButton2.setText("Вадик").setCallbackData("@VmiakoV"));
-                        keyboardButtonsRow2.add(inlineKeyboardButton3.setText("Севка").setCallbackData("@grSeva"));
-                        keyboardButtonsRow2.add(inlineKeyboardButton4.setText("Фара").setCallbackData("@faraboh"));
-                        keyboardButtonsRow3.add(inlineKeyboardButton5.setText("Сява").setCallbackData("@Rekane"));
-                        keyboardButtonsRow3.add(inlineKeyboardButton6.setText("ОБЩИЙ СБОР").setCallbackData("@allGAY"));
+                if (text.equals("/all@SixRainbowBot")) {
+                    SendMessage sendMessage = new SendMessage();
+                    sendMessage.setText("ПОГНАЛИ В РАДУГУ \uD83C\uDF08 ПАЧАНЫ: "); // sendMessage.setText("ПОГНАЛИ В РАДУГУ ПАЧАНЫ\n @LtNice @VmiakoV @Rekane @faraboh @grSeva");
+                    sendMessage.setParseMode(ParseMode.MARKDOWN);
+                    sendMessage.setChatId(message.getChatId());
 
-                        inlineButtonsList.add(keyboardButtonsRow1);
-                        inlineButtonsList.add(keyboardButtonsRow2);
-                        inlineButtonsList.add(keyboardButtonsRow3);
+                    //InlineKeyboardButton
+                    InlineKeyboardMarkup inlineKeyboardMarkup = new InlineKeyboardMarkup();
+                    List<List<InlineKeyboardButton>> inlineButtonsList = new ArrayList<>();
+                    List<InlineKeyboardButton> keyboardButtonsRow1 = new ArrayList<>();
+                    List<InlineKeyboardButton> keyboardButtonsRow2 = new ArrayList<>();
+                    List<InlineKeyboardButton> keyboardButtonsRow3 = new ArrayList<>();
 
-                        inlineKeyboardMarkup.setKeyboard(inlineButtonsList);
+                    InlineKeyboardButton inlineKeyboardButton1 = new InlineKeyboardButton();
+                    InlineKeyboardButton inlineKeyboardButton2 = new InlineKeyboardButton();
+                    InlineKeyboardButton inlineKeyboardButton3 = new InlineKeyboardButton();
+                    InlineKeyboardButton inlineKeyboardButton4 = new InlineKeyboardButton();
+                    InlineKeyboardButton inlineKeyboardButton5 = new InlineKeyboardButton();
+                    InlineKeyboardButton inlineKeyboardButton6 = new InlineKeyboardButton();
 
-                        sendMessage.setReplyMarkup(inlineKeyboardMarkup);
+                    keyboardButtonsRow1.add(inlineKeyboardButton1.setText("Дениска").setCallbackData("@LtNice"));
+                    keyboardButtonsRow1.add(inlineKeyboardButton2.setText("Вадик").setCallbackData("@VmiakoV"));
+                    keyboardButtonsRow2.add(inlineKeyboardButton3.setText("Севка").setCallbackData("@grSeva"));
+                    keyboardButtonsRow2.add(inlineKeyboardButton4.setText("Фара").setCallbackData("@faraboh"));
+                    keyboardButtonsRow3.add(inlineKeyboardButton5.setText("Сява").setCallbackData("@Rekane"));
+                    keyboardButtonsRow3.add(inlineKeyboardButton6.setText("ОБЩИЙ СБОР").setCallbackData("@allGAY"));
 
-                        try {
-                            execute(sendMessage);
-                        } catch (TelegramApiException e) {
-                            e.printStackTrace();
-                        }
+                    inlineButtonsList.add(keyboardButtonsRow1);
+                    inlineButtonsList.add(keyboardButtonsRow2);
+                    inlineButtonsList.add(keyboardButtonsRow3);
+
+                    inlineKeyboardMarkup.setKeyboard(inlineButtonsList);
+
+                    sendMessage.setReplyMarkup(inlineKeyboardMarkup);
+
+                    try {
+                        execute(sendMessage);
+                    } catch (TelegramApiException e) {
+                        e.printStackTrace();
                     }
                 }
-            } else if (update.hasCallbackQuery()) {
-                Message message = update.getCallbackQuery().getMessage();
-                CallbackQuery callbackQuery = update.getCallbackQuery();
-                String data = callbackQuery.getData();
-                SendMessage sendMessage = new SendMessage().setParseMode(ParseMode.MARKDOWN).setChatId(message.getChatId());
-
-                if(data.equals("@LtNice")){
-                    sendMessage.setText("@LtNice");
-                } else if (data.equals("@grSeva")){
-                    sendMessage.setText("@grSeva");
-                } else if (data.equals("@VmiakoV")){
-                    sendMessage.setText("@VmiakoV");
-                } else if (data.equals("@Rekane")){
-                    sendMessage.setText("@Rekane");
-                } else if (data.equals("@faraboh")){
-                    sendMessage.setText("@faraboh");
-                } else if (data.equals("@allGAY")){
-                    sendMessage.setText("@LtNice @VmiakoV @Rekane @faraboh @grSeva");
-                }
-
-                try {
-                    execute(sendMessage);
-                } catch (TelegramApiException e) {
-                    e.printStackTrace();
-                }
             }
+        } else if (update.hasCallbackQuery()) {
+            Message message = update.getCallbackQuery().getMessage();
+            CallbackQuery callbackQuery = update.getCallbackQuery();
+            String data = callbackQuery.getData();
+            SendMessage sendMessage = new SendMessage().setParseMode(ParseMode.MARKDOWN).setChatId(message.getChatId());
+
+            if (data.equals("@LtNice")) {
+                sendMessage.setText("@LtNice");
+            }
+            else if (data.equals("@grSeva")) {
+                sendMessage.setText("@grSeva");
+            }
+            else if (data.equals("@VmiakoV")) {
+                sendMessage.setText("@VmiakoV");
+            }
+            else if (data.equals("@Rekane")) {
+                sendMessage.setText("@Rekane");
+            }
+            else if (data.equals("@faraboh")) {
+                sendMessage.setText("@faraboh");
+            }
+            else if (data.equals("@allGAY")) {
+                sendMessage.setText("@LtNice @VmiakoV @Rekane @faraboh @grSeva");
+            }
+
+            try {
+                execute(sendMessage);
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     public String getBotUsername() {
@@ -98,5 +117,4 @@ public class RainbowBot extends org.telegram.telegrambots.bots.TelegramLongPolli
     public String getBotToken() {
         return "1765246757:AAH5HHdXh-iouxiP-laxvLUjixgMxcaLlCQ";
     }
-
 }
